94238d9addbc22e358c762000047c4197c836bd9
willindo <167021654+willindo@users.noreply.github.com>
2025-10-06 07:25:57 +0530

    Update axios.ts

--- lib/api/axios.ts
+++ lib/api/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ,
+  baseURL: process.env.NEXT_PUBLIC_API_URL|| "http://localhost:3001" ,
   withCredentials: true, // keep cookies/auth headers consistent
 });
 api.interceptors.request.use((config) => {
